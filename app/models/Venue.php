276441226b7eba46e7b7b8e18c1cfa25ff72acf4
Robert Bojor <robert@alttabmobile.com>
2015-02-05 12:45:53 +0200

    Debug #10

--- app/models/Venue.php
+++ app/models/Venue.php
@@ -64,15 +64,16 @@ class Venue extends Eloquent {
 
 	public static function grabStreamData($url)
 	{
-//		$ch = curl_init();
-//		curl_setopt($ch, CURLOPT_URL, $url);
-//		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//		$data = curl_exec($ch);
-//		curl_close($ch);
-//		return $data;
-		$client = new GuzzleHttp\Client();
-		$response = $client->get($url);
-		dd($response->getBody());
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		dd($data);
+		return $data;
+//		$client = new GuzzleHttp\Client();
+//		$response = $client->get($url);
+//		dd($response->getBody());
 	}
 
 	/**
