69418f7d50d83afa009a7990b05a2ebb1c2be252
Robert Bojor <robert.bojor@gmail.com>
2015-05-21 22:55:56 +0300

    Debugging calls #3.

--- app/filters.php
+++ app/filters.php
@@ -46,7 +46,7 @@ Route::filter('auth', function()
 	// 		return Redirect::guest('login');
 	// 	}
 	// }
-    dd("auth: " . Request::all());
+    dd("auth: " . json_encode(Request::all()));
 	if (!Tkn::checkToken(Request::header('Authorization'))) {
 		return Response::json(['status' => Config::get('constants.ERR_AUTH'), 'message' => Lang::get('messages.auth_error')], 401);
 	}
